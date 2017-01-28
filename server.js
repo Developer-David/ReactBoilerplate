e4fbcd48265baaee3a5133b1629c3a6db728d189
David <david.idevices@icloud.com>
2017-01-28 15:19:24 -0600

    Create Server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.static('public'));
+app.get('*', (req,res,next) => {
+    res.sendFile('index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
